79d2ce0b36363d65214cdc3d2fb152ca564c9bac
denamyte <denamyte@gmail.com>
2020-11-30 22:03:18 +0200

    2020-11-30: improved

--- Problems/Simple doubly linked list/src/Main.java
+++ Problems/Simple doubly linked list/src/Main.java
@@ -1,4 +1,8 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 class Main {
@@ -6,20 +10,16 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         LinkedList<Object> list = new LinkedList<>();
         int size = Integer.parseInt(scanner.nextLine());
-        Object[] arg = new Object[1];
-        Map<String, Runnable> cmdMap = Map.of(
-                "addFirst", () -> list.addFirst(arg[0]),
-                "addLast", () -> list.addLast(arg[0]),
-                "removeFirst", list::removeFirst,
-                "removeLast", list::removeLast,
-                "reverse", () -> Collections.reverse(list)
+        Map<String, Consumer<Object>> cmdMap = Map.of(
+                "addFirst", list::addFirst,
+                "addLast", list::addLast,
+                "removeFirst", ignored -> list.removeFirst(),
+                "removeLast", ignored -> list.removeLast(),
+                "reverse", ignored -> Collections.reverse(list)
         );
         Stream.generate(scanner::nextLine).limit(size).forEach(s -> {
-            String[] split = s.split(" ");
-            if (split.length > 1) {
-                arg[0] = split[1];
-            }
-            cmdMap.get(split[0]).run();
+            String[] arg = s.split(" ");
+            cmdMap.get(arg[0]).accept(arg[arg.length - 1]);
         });
         list.forEach(s -> System.out.printf("%s ", s));
     }
